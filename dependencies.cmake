15357b96d34b0ec13be224ab0b037e1f2afed65c
oko256 <139907514+oko256@users.noreply.github.com>
2026-03-17 12:33:54 +0200

    Separate apt depends to own env var and cache them too

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,5 +1,6 @@
 # This is an example dependency used by both targets of this example project.
 # Try to find new enough spdlog via find_package first, and if not found, fetch it.
+# Remember to add dependencies to GitHub CI workflow as well if needed: .github/workflows/ci.yml
 find_package(spdlog 1.12.0 QUIET)
 if(NOT spdlog_FOUND)
     message(STATUS "[x_PROJECT_NAME_x] System-installed spdlog not found or too old, using FetchContent")
